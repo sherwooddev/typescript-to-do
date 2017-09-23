27e89ae739db6c65d9ba2a7bfa437877d973c59a
John Sherwood <jrsherwood@gmail.com>
2017-09-23 08:30:15 -0500

    final commit

--- app/to-do-alt.ts
+++ app/to-do-alt.ts
@@ -1,11 +1,21 @@
 class Task {
   done: boolean = false;
 
-  constructor (public description: string, public priority: string) {
-    this.done = false;
+  constructor (public description: string, public priority: string) {}
+
+  markDone() {
+    this.done = true;
   }
 }
 
 var tasks: Task[] = [];
 tasks.push(new Task('Do the dishes', 'Medium'));
-console.log(tasks);
+tasks.push(new Task('Buy chocolate', 'Low'));
+tasks.push(new Task('Do laundry', 'High'));
+// console.log(tasks);
+
+tasks[0].markDone();
+
+for(var task of tasks) {
+  console.log(task);
+}
